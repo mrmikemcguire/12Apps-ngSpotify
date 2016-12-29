4b68dc023ecdb7a5c55959dba2d2831541c3cbf7
Mike McGuire <MoonshotMike@gmail.com>
2016-12-29 09:59:14 -0700

    lecture 15 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-// import { NavbarComponent } from './components/navbar/navbar.component';
-// import { SearchComponent } from './components/search/search.component';
-// import { AboutComponent } from './components/about/about.component';
-
-
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [SpotifyService]
 })
-export class AppComponent {
-title ="ngSpotify";
-}
+export class AppComponent { }
